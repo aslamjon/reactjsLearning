ea57cf87e87886de3ad1199ef912e16ceefd3131
aslamjon <aslamjon.ibragimov@gmail.com>
2021-07-21 21:09:45 +0500

    upload

---
+++ Basics/mosters-rolodex/src/components/card-list/card-list.component.jsx
@@ -0,0 +1,18 @@
+import React, { memo } from 'react';
+import { Card } from '../card/card.component';
+
+import './card-list.style.css';
+
+export const CardList = props => {
+    console.log(props);
+    return (
+        <div className="card-list"> 
+            {props.monsters.map(monster => (
+                <Card key={monster.id} monster={monster}/>
+                ))}
+        </div>
+    )
+};
+
+
+// export default CardList
