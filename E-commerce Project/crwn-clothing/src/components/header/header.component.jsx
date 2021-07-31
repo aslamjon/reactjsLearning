f6b8bd9454d0121f89fc3a4434529d547ff6768f
aslamjon ibragimov <aslamjon.ibragimov@gmail.com>
2021-07-31 01:47:47 -0900

    added cartIcon and cartDropdown components to our aplication, also implemented cart reducer

--- E-commerce Project/crwn-clothing/src/components/header/header.component.jsx
+++ E-commerce Project/crwn-clothing/src/components/header/header.component.jsx
@@ -3,10 +3,12 @@ import { Link } from 'react-router-dom';
 import { ReactComponent as Logo } from '../../assets/4.1crown.svg';
 import { connect } from 'react-redux';
 import { auth } from '../../firebase/firebase.utils'
+import CardIcon from '../card-icon/card-icon.component';
+import CartDropdown from '../cart-dropdown/cart-dropdown.component';
 
 import './header.style.scss';
 
-const Header = ({ currentUser }) => (
+const Header = ({ currentUser, hidden }) => (
     <div className="header">
         <Link className="logo-container" to='/'>
             <Logo className="logo" />
@@ -20,12 +22,15 @@ const Header = ({ currentUser }) => (
                 :
                 <Link className="option" to="/signin">SIGN IN</Link>
             }
+            <CardIcon />
         </div>
+        {hidden ? null : <CartDropdown />}
     </div>
 )
 
-const mapStateToProps = state => ({
-    currentUser: state.user.currentUser
-})
+const mapStateToProps = ({user: { currentUser }, cart: { hidden }}) => ({
+    currentUser,
+    hidden
+});
 
 export default connect(mapStateToProps)(Header);
--- E-commerce Project/crwn-clothing/src/redux/root-reducer.js
+++ E-commerce Project/crwn-clothing/src/redux/root-reducer.js
@@ -1,7 +1,8 @@
 import { combineReducers } from "redux";
 
 import userReduser from "./user/user.reducer";
-
+import cartReduser from './cart/cart.reducer';
 export default combineReducers({
-    user: userReduser
+    user: userReduser,
+    cart: cartReduser
 });
